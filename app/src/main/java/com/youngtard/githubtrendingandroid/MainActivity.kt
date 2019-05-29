fc938075b7cd747f09d9be062d59500321f384fe
youngtard <tolusotonwa@yahoo.com>
2019-05-29 17:40:23 +0100

    Initial push

---
+++ app/src/main/java/com/youngtard/githubtrendingandroid/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.youngtard.githubtrendingandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.widget.Button
+import androidx.core.text.HtmlCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val user = "youngtard"
+        val repo = "/cloupad"
+        val formatedText = "<b>$user</b>$repo"
+
+        button.setOnClickListener {
+
+
+            textView.text = HtmlCompat.fromHtml(formatedText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/youngtard/githubtrendingandroid/TrendingReposActivity.kt
@@ -0,0 +1,12 @@
+package com.youngtard.githubtrendingandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrendingReposActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trending_repos)
+    }
+}
